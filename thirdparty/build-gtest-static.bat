f316ef83373470764ffbcb0c2b65dcf8cab0e3b0
frankee <frankee.zhou@gmail.com>
2011-07-20 21:46:19 +0800

    using the md version of the gtest.

--- thirdparty/build-gtest-static.bat
+++ thirdparty/build-gtest-static.bat
@@ -3,14 +3,14 @@ set VS_PATH=c:\Program Files\Microsoft Visual Studio 8
 call "%VS_PATH%\VC\vcvarsall.bat"
 
 echo clean
-devenv gtest-1.6.0\msvc\gtest.sln /clean "Debug|Win32"
-devenv gtest-1.6.0\msvc\gtest.sln /clean "Release|Win32"
+devenv gtest-1.6.0\msvc\gtest-md.sln /clean "Debug|Win32"
+devenv gtest-1.6.0\msvc\gtest-md.sln /clean "Release|Win32"
 
 echo compile
-devenv gtest-1.6.0\msvc\gtest.sln /rebuild "Debug|Win32"
-devenv gtest-1.6.0\msvc\gtest.sln /rebuild "Release|Win32"
+devenv gtest-1.6.0\msvc\gtest-md.sln /rebuild "Debug|Win32"
+devenv gtest-1.6.0\msvc\gtest-md.sln /rebuild "Release|Win32"
 
-xcopy gtest-1.6.0\msvc\gtest\Debug\gtestd.lib ..\lib\gtestd.lib
-xcopy gtest-1.6.0\msvc\gtest\Debug\gtest_maind.lib ..\lib\gtest_maind.lib
-xcopy gtest-1.6.0\msvc\gtest\Release\gtest.lib ..\lib\gtest.lib
-xcopy gtest-1.6.0\msvc\gtest\Release\gtest_main.lib ..\lib\gtest_main.lib
+xcopy gtest-1.6.0\msvc\gtest-md\Debug\gtestd.lib ..\lib\
+xcopy gtest-1.6.0\msvc\gtest-md\Debug\gtest_main-mdd.lib ..\lib\
+xcopy gtest-1.6.0\msvc\gtest-md\Release\gtest.lib ..\lib\
+xcopy gtest-1.6.0\msvc\gtest-md\Release\gtest_main-md.lib ..\lib\
